82ad6503066fa5f7cabab72dc97cd9b8f18f44f5
Adit Namdev <anamdev@ece001.campus.ece.cmu.local>
2016-09-05 22:54:48 -0400

    added 7-seg display

--- p1/p1.sv
+++ p1/p1.sv
@@ -19,5 +19,38 @@ module p1 (
                     .done(done), .sum(sum));
 
   downStream dsThread(.potVal(sum), .load(done), .clk(CLOCK_50), .rst_l(BUTTON2), .sum(sumfinalOut));
+
+  sevenSegmentDisplay tb1Value(.valueToDisplay(tbSum[7:4]), .hexDisplay(HEX3));
+  sevenSegmentDisplay tb2Value(.valueToDisplay(tbSum[3:0]), .hexDisplay(HEX2));
+  sevenSegmentDisplay ds1Value(.valueToDisplay(sumfinalOut[7:4]), .hexDisplay(HEX1));
+  sevenSegmentDisplay ds2Value(.valueToDisplay(sumfinalOut[3:0]), .hexDisplay(HEX0));
   
-endmodule
+endmodule: p1
+
+
+module sevenSegmentDisplay(
+  input logic [3:0] valueToDisplay,
+  output logic [6:0] hexDisplay);
+
+  always_comb begin
+    case (valueToDisplay)
+      4'd0:  hexDisplay = 7'b0000001;
+      4'd1:  hexDisplay = 7'b1001111;
+      4'd2:  hexDisplay = 7'b0010010;
+      4'd3:  hexDisplay = 7'b0000110;
+      4'd4:  hexDisplay = 7'b1001100;      
+      4'd5:  hexDisplay = 7'b0100100;
+      4'd6:  hexDisplay = 7'b0100000;
+      4'd7:  hexDisplay = 7'b0001111;
+      4'd8:  hexDisplay = 7'b0000000;
+      4'd9:  hexDisplay = 7'b0001100;
+      4'd10: hexDisplay = 7'b0001000;
+      4'd11: hexDisplay = 7'b1100000;
+      4'd12: hexDisplay = 7'b0110001;
+      4'd13: hexDisplay = 7'b1000010;
+      4'd14: hexDisplay = 7'b0110000;
+      4'd15: hexDisplay = 7'b0111000;
+    endcase
+  end
+
+endmodule: sevenSegmentDisplay
